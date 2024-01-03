029364272df376800fe24a0049d900e5a2660d50
Rob Killick <robkillick@me.com>
2024-01-03 09:25:16 +0000

    feat: add views, serializer and urls for profiles

--- profiles/models.py
+++ profiles/models.py
@@ -3,6 +3,7 @@ from django.db.models.signals import post_save
 from django.contrib.auth.models import User
 
 
+
 class Profile(models.Model):
     owner = models.OneToOneField(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rewind.permissions import IsOwnerOrReadOnly
+from .models import Profile
+from .serializers import ProfileSerializer
 
-# Create your views here.
+
+class ProfileList(generics.ListAPIView):
+    """
+    Lists all profiles.
+    No Create view required, as profile creation handled by Django signals.
+    """
+    queryset = Profile.objects.all()
+    serializer_class = ProfileSerializer
+
+class ProfileDetail(generics.RetrieveUpdateAPIView):
+    """
+    Retrieve or update profile.  The profile can only
+    be updated if the user is the owner.
+    """
+
+    permission_classes = [IsOwnerOrReadOnly]
+    queryset = Profile.objects.all()
+    serializer_class = ProfileSerializer
