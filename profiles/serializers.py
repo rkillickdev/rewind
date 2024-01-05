8fd13250dcb182c4c6636335b321b54ef1e6c425
Rob Killick <robkillick@me.com>
2024-01-05 09:44:36 +0000

    feat: add get_following_id method to ProfileSerializer class

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import Profile
+from followers.models import Follower
 
 
 class ProfileSerializer(serializers.ModelSerializer):
@@ -15,10 +16,20 @@ class ProfileSerializer(serializers.ModelSerializer):
 
     owner = serializers.ReadOnlyField(source="owner.username")
     is_owner = serializers.SerializerMethodField()
+    following_id = serializers.SerializerMethodField()
 
     def get_is_owner(self, obj):
         request = self.context["request"]
         return request.user == obj.owner
+
+    def get_following_id(self, obj):
+        user = self.context["request"].user
+        if user.is_authenticated:
+            following = Follower.objects.filter(
+                owner=user, followed=obj.owner    
+            ).first()
+            return following.id if following else None
+        return None
 
     class Meta:
         model = Profile
@@ -34,6 +45,7 @@ class ProfileSerializer(serializers.ModelSerializer):
             "era_preferences",
             "genre_preferences",
             "category_preferences",
+            "following_id",
         ]
 
 
