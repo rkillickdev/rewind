16512c8121d04ba2b4b1269757ffd5751f7b8b25
Rob Killick <robkillick@me.com>
2024-01-10 19:21:21 +0000

    feat: add NavBar component and basic css styling

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,13 +1,15 @@
 import React from "react";
 import { Navbar, Container, Nav } from "react-bootstrap";
 import logo from "../assets/cassette-orange-logo.png";
+import styles from "../styles/NavBar.module.css";
 
 const NavBar = () => {
   return (
-    <Navbar expand="md" fixed="top">
+    <Navbar className={styles.NavBar} expand="md" fixed="top">
       <Container>
-        <Navbar.Brand>
-          <img src={logo} alt="casette tape logo" height="45" />
+        <Navbar.Brand className="d-flex align-items-center">
+          <img src={logo} alt="casette tape logo" height="60" />
+          <h1>Rewind</h1>
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
