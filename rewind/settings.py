87e80fc0732c3aca516b62868a71b27fba6b9423
Rob Killick <robkillick@me.com>
2024-01-10 11:43:46 +0000

    chore: modify env variable used to connect to development database

--- rewind/settings.py
+++ rewind/settings.py
@@ -141,18 +141,19 @@ WSGI_APPLICATION = 'rewind.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-if 'DEV' in os.environ:
+if 'DEV_DB' in os.environ:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
             'NAME': BASE_DIR / 'db.sqlite3',
         }
     }
+    print('dev database connected')
 else:
     DATABASES = {
         'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
     }
-
+    print('production database connected')
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
