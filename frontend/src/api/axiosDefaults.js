cb050159255597c8d5fcc832889c95f27abd3829
Rob Killick <robkillick@me.com>
2024-01-12 00:03:02 +0000

    feat: check log in status using custom context hook and create interceptors to check access tokens

--- frontend/src/api/axiosDefaults.js
+++ frontend/src/api/axiosDefaults.js
@@ -10,3 +10,6 @@ import axios from "axios";
 // axios.defaults.baseURL = ''
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
+
+export const axiosReq = axios.create();
+export const axiosRes = axios.create();
