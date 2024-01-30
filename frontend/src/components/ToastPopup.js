f005f6b3716b5958659875920bacfd6e6460c642
Rob Killick <robkillick@me.com>
2024-01-30 00:06:51 +0000

    feat: enable manual close of toast when clicking cross icon

--- frontend/src/components/ToastPopup.js
+++ frontend/src/components/ToastPopup.js
@@ -1,13 +1,26 @@
 import { Toast } from "react-bootstrap";
 import useAlert from "../hooks/useAlert";
 import styles from "../styles/ToastPopup.module.css";
+import { useEffect, useState } from "react";
 
 const ToastPopup = () => {
   const { text, type } = useAlert();
+  const [showToast, setShowToast] = useState(true);
+  const toggleToast = () => setShowToast(!showToast);
+
+  useEffect(() => {
+    setShowToast(true);
+  }, [useAlert()]);
 
   if (text && type === "success") {
     return (
-      <Toast className={styles.ToastFixed}>
+      <Toast
+        className={styles.ToastFixed}
+        show={showToast}
+        onClose={toggleToast}
+        delay={3000}
+        autohide
+      >
         <Toast.Header>
           <i className="fa-solid fa-circle-check mr-auto"></i>
         </Toast.Header>
