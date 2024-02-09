ec8ebc31dfba1ab00ce5313c52c877253e086923
Rob Killick <robkillick@me.com>
2024-02-09 10:45:46 +0000

    style: add comments and tidy imports for AlertPopup component

--- frontend/src/components/AlertPopup.js
+++ frontend/src/components/AlertPopup.js
@@ -1,10 +1,13 @@
 import { Alert } from "react-bootstrap";
 import useAlert from "../hooks/useAlert";
+
 import styles from "../styles/AlertPopup.module.css";
 
 const AlertPopup = () => {
+  // Destructure text and type from the useAlert custom hook
   const { text, type } = useAlert();
 
+  // If text and type exist, render an Alert component
   if (text && type === "warning") {
     return (
       <Alert className={styles.AlertFixed} variant={type}>
@@ -12,6 +15,7 @@ const AlertPopup = () => {
       </Alert>
     );
   } else {
+    // If text or type missing, render an empty fragment
     return <></>;
   }
 };
