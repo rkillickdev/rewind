403481e378edc846ffd9fe58593cc7103593b5ae
Rob Killick <robkillick@me.com>
2024-02-09 15:11:56 +0000

    style: add comment

--- frontend/src/hooks/useAlert.js
+++ frontend/src/hooks/useAlert.js
@@ -1,6 +1,12 @@
 import { useContext } from "react";
 import AlertContext from "../contexts/AlertContext";
 
+/*
+  Custom hook that uses the AlertContext and can be called
+  throughout the app to provide alert functionality when
+  required.
+*/
+
 const useAlert = () => useContext(AlertContext);
 
 export default useAlert;
