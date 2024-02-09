4fa0e6e44b317405be599ff8e2fa160e0ccfce34
Rob Killick <robkillick@me.com>
2024-02-09 12:22:29 +0000

    style: tidy format  and imports.  Add comments for ToastPopup component

--- frontend/src/components/ToastPopup.js
+++ frontend/src/components/ToastPopup.js
@@ -1,17 +1,22 @@
-import { Toast } from "react-bootstrap";
+import Toast from "react-bootstrap/Toast";
 import useAlert from "../hooks/useAlert";
 import styles from "../styles/ToastPopup.module.css";
 import { useEffect, useState } from "react";
 
 const ToastPopup = () => {
+  // Destructure text and type from the useAlert custom hook
   const { text, type } = useAlert();
+
   const [showToast, setShowToast] = useState(true);
   const toggleToast = () => setShowToast(!showToast);
 
+  // useEffect hook with callback function that sets value of showToast to true
+  // Callback function triggered whenever the useAlert function is called
   useEffect(() => {
     setShowToast(true);
   }, [useAlert()]);
 
+  // If text and type is equal to the string 'success', render a Toast component
   if (text && type === "success") {
     return (
       <Toast
@@ -29,6 +34,7 @@ const ToastPopup = () => {
       </Toast>
     );
   } else {
+    // If text or type missing, render an empty fragment
     return <></>;
   }
 };
