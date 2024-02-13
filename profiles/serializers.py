2a73fb66be967ca545da9c5135bd85f282ca0d6b
Rob Killick <robkillick@me.com>
2024-02-13 17:23:23 +0000

    feat: add image validation to profile serializer

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -28,6 +28,21 @@ class ProfileSerializer(serializers.ModelSerializer):
     followers_count = serializers.ReadOnlyField()
     following_count = serializers.ReadOnlyField()
 
+    def validate_image(self, value):
+        if value.size > 2 * 1024 * 1024:
+            raise serializers.ValidationError(
+                "Please choose an image smaller than 2MB"
+            )
+        if value.image.height > 4096:
+            raise serializers.ValidationError(
+                "Image height larger than 4096px!"
+            )
+        if value.image.width > 4096:
+            raise serializers.ValidationError(
+                "Image width larger than 4096px!"
+            )
+        return value
+
     def get_is_owner(self, obj):
         request = self.context["request"]
         return request.user == obj.owner
