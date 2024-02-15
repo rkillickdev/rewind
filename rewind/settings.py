efb6c632bfe835bf2bf93972570de56503d6128c
Rob Killick <robkillick@me.com>
2024-02-15 21:44:58 +0000

    feat: change pagination page size to 5

--- rewind/settings.py
+++ rewind/settings.py
@@ -34,7 +34,7 @@ REST_FRAMEWORK = {
     )],
     'DEFAULT_PAGINATION_CLASS':
         'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10,
+    'PAGE_SIZE': 5,
     'DATETIME_FORMAT': '%d %b %Y',
 }
 if 'DEV' not in os.environ:
